66c259c80744c2a974d74668f8fc70c6aaed64db
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-11-26 14:29:29 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

--- public/js/app.js
+++ public/js/app.js
@@ -26,8 +26,7 @@ function calculateCenter() {
 function loadGoogleMap() {
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
-	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&'
-			+ 'callback=initialize';
+	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
 	document.body.appendChild(script);
 }
 
