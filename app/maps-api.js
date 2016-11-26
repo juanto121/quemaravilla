97edc83f0b43d03523f6c0e81103a57da8a61f12
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-11-26 18:28:22 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

---
+++ app/maps-api.js
@@ -0,0 +1,20 @@
+/**
+ * 
+
+var googleMapsClient = require('@google/maps').createClient({
+	key : 'AIzaSyBfXWyGvlk7JQDZBZTpQWoA0QQzaXWhY0Q'
+}); */
+
+var gmaputil = require('googlemapsutil');
+
+// call api from class object
+var cb = function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+};
+
+exports.calculateAndDisplayRoute = function(query, callback) {
+	return gmaputil.directions(query.origin, query.destination, null, cb);
+}
